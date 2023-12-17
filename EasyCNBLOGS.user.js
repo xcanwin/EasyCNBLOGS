0b4382a23467b14edc8b44b300273e573e2226fb
xcanwin <xcanwin@gmail.com>
2023-12-17 15:47:43 +0800

    新增超净化页面功能

--- EasyCNBLOGS.user.js
+++ EasyCNBLOGS.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EasyCNBLOGS
 // @description  这是一款促进博客园极致简洁和高效的插件。免费共享大量创新功能，如：净化页面等。让我们的学习体验无比简洁、专注、高效、畅快。
-// @version      1.1
+// @version      2.0
 // @author       xcanwin
 // @namespace    https://github.com/xcanwin/EasyCNBLOGS/
 // @supportURL   https://github.com/xcanwin/EasyCNBLOGS/
@@ -22,29 +22,55 @@
 
     /*电脑端净化样式*/
     const purify_style_pc = `
-#top_nav /*隐藏[置顶的][顶部的]菜单栏*/,
-#top /*隐藏[置顶的][顶部的]菜单栏2*/,
-#mynavbar /*隐藏[置顶的][顶部的]菜单栏3*/,
-#navigator /*隐藏[置顶的][顶部的]菜单栏4*/,
-#mytopmenu /*隐藏[置顶的][顶部的]菜单栏5*/,
-#header /*隐藏[正文的][顶部的]用户介绍*/,
-#leftmenu /*隐藏[左边的]栏*/,
-#leftcontent /*隐藏[左边的]栏2*/,
-#sideBar /*隐藏[右边的]栏*/,
-#right_meun /*隐藏[右边的]栏2*/,
-#blog_post_info_block /*隐藏[正文的][底部的]推荐关注*/,
-#comment_form /*隐藏[正文的][底部的]登录提示和推荐文章*/,
-#footer /*隐藏[正文的][底部的]powered by*/,
-.footer /*隐藏[正文的][底部的]powered by2*/
+#blog_post_info_block /*隐藏[正文的][底部的]推荐关注*/
 {
     display: none !important;
 }
+
+/*隐藏背景*/
+html body {
+    background: none !important;
+    background-image: unset !important;
+    background-color: unset !important;
+}
+
+/*展示全屏*/
+body {
+    margin-left: unset !important;
+    margin-right: unset !important;
+    margin-bottom: unset !important;
+    padding: unset !important;
+    padding-bottom: unset !important;
+    display: flex;
+    justify-content: center;
+}
+.post {
+    border: unset !important;
+    border-bottom-width: unset !important;
+    border-right-width: unset !important;
+    padding: unset !important;
+    margin-bottom: unset !important;
+    width: 80%;
+}
+
+
 `;
 
     //净化页面
     const purifyPage = function() {
         GM_addStyle(purify_style_pc);
     };
+
+    //超净化页面
+    const purifyPageSuper = function() {
+        const p = $(".post");
+        $('body').innerHTML = "";
+        $('body').appendChild(p);
+    };
+
+    document.addEventListener("DOMContentLoaded", function() {
+        purifyPageSuper();
+    });
 
     purifyPage();
 
