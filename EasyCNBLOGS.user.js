32c3415d33e7695b7025bbfe253ea78fc5abfbe8
xcanwin <xcanwin@gmail.com>
2023-12-17 16:08:22 +0800

    新增调整标题

--- EasyCNBLOGS.user.js
+++ EasyCNBLOGS.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         EasyCNBLOGS
 // @description  这是一款促进博客园极致简洁和高效的插件。免费共享大量创新功能，如：净化页面等。让我们的学习体验无比简洁、专注、高效、畅快。
-// @version      2.0
+// @version      3.0
 // @author       xcanwin
 // @namespace    https://github.com/xcanwin/EasyCNBLOGS/
 // @supportURL   https://github.com/xcanwin/EasyCNBLOGS/
@@ -22,6 +22,7 @@
 
     /*电脑端净化样式*/
     const purify_style_pc = `
+.postTitle /*隐藏[正文的][顶部的]原标题*/,
 #blog_post_info_block /*隐藏[正文的][底部的]推荐关注*/
 {
     display: none !important;
@@ -52,8 +53,23 @@ body {
     margin-bottom: unset !important;
     width: 80%;
 }
+.postText {
+    padding-right: unset !important;
+    padding-left: unset !important;
+    padding-bottom: unset !important;
+    padding-top: unset !important;
+}
 
-
+/*调整标题*/
+span[role="heading"] {
+    display: flex;
+    justify-content: center;
+    margin-top: 35px !important;
+    margin-bottom: 20px !important;
+    color: black !important;
+    font-size: 33px !important;
+    font-family: 'PingFang SC','Microsoft YaHei','SimHei','Arial','SimSun';
+}
 `;
 
     //净化页面
@@ -68,8 +84,14 @@ body {
         $('body').appendChild(p);
     };
 
+    //调整标题
+    const beautyTitle = function() {
+        $('.post').insertBefore($('span[role="heading"]'), $('.post').childNodes[0]);
+    };
+
     document.addEventListener("DOMContentLoaded", function() {
         purifyPageSuper();
+        beautyTitle();
     });
 
     purifyPage();
